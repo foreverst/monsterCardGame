785c906d9a8205795ce561a393999e6a376993cb
foreverst <41577715+foreverst@users.noreply.github.com>
2020-03-19 10:09:47 +0800

    Create monster.cpp

---
+++ monster.cpp
@@ -0,0 +1,17 @@
+#include "monster.h"
+#include <string>
+
+
+monster::monster()
+{
+	//this->name = name;
+	//this->atk = atk;
+	//this->def = def;
+	//this->type = type;
+	
+}
+
+
+monster::~monster()
+{
+}
